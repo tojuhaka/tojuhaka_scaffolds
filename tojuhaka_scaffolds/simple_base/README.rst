b2bff57730d5520368226109e798889599427980
Toni Haka-Risku <tojuhaka@gmail.com>
2012-03-30 11:13:13 +0300

    tojuhaka_scaffolds renamed to scaffolds

--- tojuhaka_scaffolds/simple_base/README.rst
+++
@@ -1,1 +0,0 @@
-tojuhaka README
--- tojuhaka_scaffolds/zodb_users/+package+/interfaces.py
+++
@@ -1,5 +0,0 @@
-from zope.interface import Interface
-
-class ISite(Interface):
-    """ Marker interface. All the site objects must implement
-    this interaface."""
